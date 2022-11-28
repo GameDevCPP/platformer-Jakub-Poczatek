5842e68accb3de0ea8eaa8fab0e9c20814fccac5
Jakub Poczatek <kubapoczatek2001@gmail.com>
2022-11-28 22:48:21 +0000

    Level 2 and 3 Complete.

--- lab_7_platformer/scenes/scene_level2.cpp
+++ lab_7_platformer/scenes/scene_level2.cpp
@@ -19,14 +19,14 @@ void Level2Scene::Load() {
 
   // Create player
   {
-    // *********************************
 
+    player = makeEntity();
+    player->setPosition(ls::getTilePosition(ls::findTiles(ls::START)[0]));
+    auto s = player->addComponent<ShapeComponent>();
+    s->setShape<sf::RectangleShape>(Vector2f(20.f, 30.f));
+    s->getShape().setFillColor(Color::Magenta);
+    s->getShape().setOrigin(Vector2f(10.f, 15.f));
 
-
-
-
-
-    // *********************************
     player->addTag("player");
     player->addComponent<PlayerPhysicsComponent>(Vector2f(20.f, 30.f));
   }
@@ -38,15 +38,16 @@ void Level2Scene::Load() {
                        Vector2f(0, 24));
     // *********************************
     // Add HurtComponent
-
     // Add ShapeComponent, Red 16.f Circle
-
-
-
-
     // Add EnemyAIComponent
-
     // *********************************
+    enemy->addComponent<HurtComponent>();
+    auto s = enemy->addComponent<ShapeComponent>();
+    s->setShape<sf::CircleShape>(16.f);
+    s->getShape().setFillColor(Color::Red);
+    s->getShape().setOrigin(Vector2f(16.f, 16.f));
+
+    enemy->addComponent<EnemyAIComponent>();
   }
 
   // Create Turret
@@ -64,17 +65,16 @@ void Level2Scene::Load() {
   // Add physics colliders to level tiles.
   {
     // *********************************
-
-
-
-
-
-
-
-
+    auto walls = ls::findTiles(ls::WALL);
+    for (auto w: walls){
+        auto pos = ls::getTilePosition(w);
+        pos += Vector2f(20.f, 20.f);
+        auto e = makeEntity();
+        e->setPosition(pos);
+        e->addComponent<PhysicsComponent>(false, Vector2f(40.f, 40.f));
+    }
     // *********************************
   }
-
   cout << " Scene 2 Load Done" << endl;
   setLoaded(true);
 }
